5dfb2d583a7611abb4bbaa3d0c47ae16154f51e1
Depayit by jacob <copternuttapong@gmail.com>
2026-01-29 19:31:44 +0700

    Update main.py

--- Depayit-Project/main.py
+++ Depayit-Project/main.py
@@ -85,5 +85,3 @@ def read_root():
 # บอก FastAPI ว่าไฟล์ HTML อยู่ในโฟลเดอร์ static นะ
 app.mount("/static", StaticFiles(directory="static", html=True), name="static")
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
